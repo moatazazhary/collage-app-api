22d13505656e4c867d83acade0a267d0b174c40c
Moataz Azhary <248873415+moatazazhary@users.noreply.github.com>
2026-02-15 22:05:36 +0300

    Delete prisma/migrations directory

--- prisma/migrations/20251230061934_update_fileds_in_exam_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Exam" DROP CONSTRAINT "Exam_fileId_fkey";
-
--- AlterTable
-ALTER TABLE "Exam" ALTER COLUMN "fileId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Exam" ADD CONSTRAINT "Exam_fileId_fkey" FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20260107094707_edit_degree_request_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `certificateType` on the `DegreeRequest` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "DegreeRequest" DROP COLUMN "certificateType";
